9e493ec7b487cd7a3b9ff662faa4b2d5a8faf948
KatieRina <eashchukina@yandex.ru>
2022-12-19 01:04:47 +0300

    add 4th

--- HW1/HW_4.py
+++ HW1/HW_4.py
@@ -4,4 +4,15 @@
 # - 1 -> x > 0, y > 0
 # - 8 -> нет такой четверти
 
-
+quarter = int(input())
+while quarter < 1 or quarter > 4:
+    print("Это не номер четверти! Попробуйте ещё раз:")
+    quarter = int(input())
+if quarter == 1:
+    print("x > 0, y > 0")
+elif quarter == 2:
+    print("x < 0, y > 0")
+elif quarter == 3:
+    print("x < 0, y < 0")
+elif quarter == 4:
+    print("x > 0, y < 0")      
