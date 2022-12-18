10e11308d4bcff3cbac4d8ff1e17897afe3c689c
KatieRina <eashchukina@yandex.ru>
2022-12-19 00:56:42 +0300

    add 3rd

--- HW1/HW_3.py
+++ HW1/HW_3.py
@@ -7,3 +7,26 @@
 # - x=2; y=4-> 1
 # - x=-34; y=-30 -> 3
 
+x = int(input())
+y = int(input())
+
+if x==0 and y==0:
+    print("начало координат")
+elif x == 0 and y > 0:
+    print("ось y, между I и II четвертями")
+elif x == 0 and y < 0:
+    print("ось y, между III и IV четвертями")
+elif y == 0 and x > 0:
+    print("ось x, между I и IV четвертями")  
+elif y == 0 and x < 0:
+    print("ось x, между II и III четвертями")      
+elif x > 0 and y > 0:
+    print("I")
+elif x < 0 < y:
+    print("II")
+elif x < 0 and y < 0:
+    print("III")
+elif x > 0 > y:
+    print("IV")  
+
+
